5cebc250b9977a054c44213fab403616d313c237
michaelmateju <mysakbm@gmail.com>
2024-07-16 14:18:00 +0200

    MSG: Utils file

---
+++ utils.py
@@ -0,0 +1,10 @@
+import time
+
+def time_run(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Function {func.__name__} took {end - start} seconds.")
+        return result
+    return wrapper
